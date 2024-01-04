c1a8f51856949af578aa2148a579ce904e2ba7e5
Lim Rocky <lim.rocky@cashwalk.io>
2024-01-04 17:10:50 +0900

    initial setting

---
+++ GoodChatting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodChatting
+//
+//  Created by Rocky on 1/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .gray
+    }
+
+
+}
+
