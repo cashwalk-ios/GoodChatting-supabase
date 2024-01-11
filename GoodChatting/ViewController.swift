ea6137a6ba333d051d8559dda520439fd66de1b0
ios-kkr <kim.kwangrok@cashwalk.io>
2024-01-11 13:55:56 +0900

    Add Sample code

--- GoodChatting/ViewController.swift
+++ GoodChatting/ViewController.swift
@@ -6,15 +6,33 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+import RxSwift
 
 class ViewController: UIViewController {
+    
+    private var helloLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupProperties()
+        setupView()
+    }
+    
+    private func setupProperties() {
         self.view.backgroundColor = .designColor(color: .secondGray())
     }
 
+    private func setupView() {
+        helloLabel = UILabel().then {
+            $0.text = "Hello, World!"
+            view.addSubview($0)
+            $0.snp.makeConstraints { make in
+                make.center.equalToSuperview()
+            }
+        }
+    }
 
 }
 
