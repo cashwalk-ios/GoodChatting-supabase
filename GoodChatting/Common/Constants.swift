d0b50844c861018ea028f39f177e53c56c8b0b8e
ios-kkr <kim.kwangrok@cashwalk.io>
2024-01-12 14:02:17 +0900

    Add Constants

---
+++ GoodChatting/Common/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  GoodChatting
+//
+//  Created by 김광록 on 1/12/24.
+//
+
+import Foundation
+
+enum Constants {
+  static let KAKAO_APP_KEY = "deb2294cd579459347b96ed330699075"
+  static let KAKAO_CLIENT_ID = "d6f2d0ca38ce5a55513d057a6ff0997d"
+  static let KAKAO_CLIENT_SECRET = "PVxHUzxgyjfkyXCKDcS0TTGBJVg6eFwj"
+  static let KAKAO_REDIRECT_URL = "https://flphwibweztgdzwqnjdp.supabase.co/auth/v1/callback"
+}
