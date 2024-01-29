fc4408ae34082e88a36842f1791f285b8e457bb2
김태욱 <nous178@cashwalk.io>
2024-01-30 08:51:59 +0900

    feat: 채팅방 파일 생성

---
+++ GoodChatting/Scenes/Chat/ChatReactor.swift
@@ -0,0 +1,35 @@
+//
+//  ChatReactor.swift
+//  GoodChatting
+//
+//  Created by kimtaeuk-N275 on 1/23/24.
+//
+
+import Foundation
+import ReactorKit
+
+final class ChatReactor: Reactor {
+    
+    enum Action {
+        
+    }
+    
+    enum Mutation {
+        
+    }
+    
+    struct State {
+        
+    }
+    
+    let initialState = State()
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        
+    }
+    
+}
---
+++ GoodChatting/Scenes/Chat/ChatViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ChatViewController.swift
+//  GoodChatting
+//
+//  Created by kimtaeuk-N275 on 1/23/24.
+//
+
+import UIKit
+import ReactorKit
+
+class ChatViewController: UIViewController, View {
+    
+    var disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func bind(reactor: ChatReactor) {
+        
+    }
+}
